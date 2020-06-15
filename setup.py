b2ebec7d41e690acb3d530251fbc8d92392ec5d6
IvarWithoutBones <ivar.scholten@protonmail.com>
2020-06-15 19:52:36 +0200

    add setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='i3blocks-spotify',
+   version='1.0',
+   description='Script to display the currently playing song from either spotify or ncspot',
+   author='IvarWithoutBones & firatakandere',
+   author_email='ivar.scholten@protonmail.com',
+   packages=['.'],
+)
